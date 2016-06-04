e435c36c3dbe27bf0177f66b06ac6499dcdd3a0d
Alex <alex@alisterdev.com>
2016-06-04 11:29:40 -0400

    test listener

--- _build/gstella_bot.js
+++ _build/gstella_bot.js
@@ -18,5 +18,9 @@ controller.spawn({
   }
 });
 
+controller.hears(['hello','hi'],['direct_message','direct_mention','mention'],function(bot,message) {
+    bot.reply(message,"Hello.");
+});
+
 require('./teams.js');
 require('./placeBet.js');
