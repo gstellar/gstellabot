82466cd3b3c0fc37f84249dfacf7d3abdf06d84a
Alex <alex@alisterdev.com>
2016-06-04 10:58:55 -0400

    set up team env

--- _build/gstella_bot.js
+++ _build/gstella_bot.js
@@ -1,10 +1,22 @@
-var Botkit = require('botkit');
-var controller = Botkit.slackbot();
-var bot = controller.spawn({
-  token: my_slack_bot_token
-})
-bot.startRTM(function(err,bot,payload) {
+var Botkit = require('../lib/Botkit.js');
+
+
+if (!process.env.token) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+var controller = Botkit.slackbot({
+ debug: false
+});
+
+controller.spawn({
+  token: process.env.token
+}).startRTM(function(err) {
   if (err) {
-    throw new Error('Could not connect to Slack');
+    throw new Error(err);
   }
 });
+
+require('./teams.js');
+require('./placeBet.js');
